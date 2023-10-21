10320f8cad3e7e13bd5e48a4af0b8ae312ad033e
Crowdbotics <team@crowdbotics.com>
2023-10-21 02:25:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shy-unit-44078",
+  url: "https://shy-unit-44078.botics.co",
+  api: "https://shy-unit-44078.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
